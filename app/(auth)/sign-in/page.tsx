4ce68baf02f66b36dd68ccc19d234f76dc21b44c
Daniel Hurtado <a01707774@tec.mx>
2025-10-21 21:49:28 -0500

    feat(deps): add @radix-ui/react-label, @radix-ui/react-select, and react-hook-form dependencies

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+function SignInPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Sign In</h1>
+    </div>
+  );
+}
+
+export default SignInPage;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+function SignUp() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Sign Up</h1>
+    </div>
+  );
+}
+
+export default SignUp;
