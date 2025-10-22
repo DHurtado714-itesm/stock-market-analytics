24515dd555c344646e8db4957dfeb5139a060a4e
Daniel Hurtado <a01707774@tec.mx>
2025-10-21 22:03:17 -0500

    feat(sign-up): implement sign-up form with validation using react-hook-form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,80 @@
+"use client";
+
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+
 function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "US",
+      investmentGoals: "Growth",
+      riskTolerance: "Medium",
+      preferredIndustry: "Technology",
+    },
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data: SignUpFormData) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Sign Up</h1>
-    </div>
+    <>
+      <h1 className="form-title">Sign Up & Personalize</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="Enter your full name"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full name is required", minLength: 2 }}
+        />
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="jordan@strattonoakmont.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email is required",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Invalid email",
+            },
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 8 }}
+        />
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Creating Account" : "Start Your Investing Journey"}
+        </Button>
+      </form>
+    </>
   );
 }
 
---
+++ components/forms/InputField.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+function InputField({
+  name,
+  label,
+  placeholder,
+  register,
+  type = "text",
+  error,
+  validation,
+  disabled = false,
+}: FormInputProps) {
+  return (
+    <div className="space-y-2">
+      <Label htmlFor={name} className="form-label">
+        {label}
+      </Label>
+      <Input
+        type={type}
+        id={name}
+        placeholder={placeholder}
+        error={!!error}
+        className={cn("form-input", {
+          "opacity-50 cursor-not-allowed": disabled,
+        })}
+        {...register(name, validation)}
+      />
+      {error && <p className="text-sm text-red-500">{error.message}</p>}
+    </div>
+  );
+}
+
+export default InputField;
