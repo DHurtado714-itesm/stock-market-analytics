0dd4b45c32d0a35956832c4e60badd7a6d6feb8e
Daniel Hurtado <a01707774@tec.mx>
2025-10-23 15:23:00 -0500

    feat(email): add sendWelcomeEmail function to send personalized welcome emails

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { WELCOME_EMAIL_TEMPLATE } from "./template";
 
 export const transporter = () => {
   return nodemailer.createTransport({
@@ -8,4 +9,29 @@ export const transporter = () => {
       pass: process.env.NODEMAILER_PASSWORD,
     },
   });
+};
+
+export const sendWelcomeEmail = async ({
+  email,
+  name,
+  intro,
+}: WelcomeEmailData) => {
+  const htmlTemplate = WELCOME_EMAIL_TEMPLATE.replace(
+    "{{intro}}",
+    intro
+  ).replace("{{name}}", name);
+
+  const mailOptions = {
+    from: `"Signalist" <${process.env.NODEMAILER_EMAIL}>`,
+    to: email,
+    subject: "Welcome to Signalist - Your Stock Market Dashboard is Ready!",
+    html: htmlTemplate,
+  };
+
+  try {
+    await transporter().sendMail(mailOptions);
+  } catch (error) {
+    console.error("Error sending welcome email:", error);
+    throw error;
+  }
 };
